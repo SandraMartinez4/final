0e3939e9f2877ecdc311e93a45368f2ca50b1364
SANDRA MARTINEZ <sandramartinez@IdeaPad-Slim-3-15IRU8-f8a7010a.localdomain>
2025-10-24 00:28:51 +0000

    agr

--- arithmetic/expr_api/settings.py
+++ arithmetic/expr_api/settings.py
@@ -80,8 +80,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# Default primary key field type
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
